07b8d55dfbaa2e947dada00a417ecb0e5650a62c
Emiliano Alonzo <emialonzo@gmail.com>
2016-06-23 14:31:49 -0300

    cambiando permisos de code

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 
 
 sudo chown  -R $USER:$USER code
-sudo chmod -R 777 code
+
 
 wget -q http://sourceforge.net/projects/testlink/files/TestLink%201.9/TestLink%201.9.14/testlink-1.9.14.tar.gz/download -O testlink-1.9.14.tar.gz &&\
     tar zxvf testlink-1.9.14.tar.gz && \
     mv testlink-1.9.14/ code/testlink code
+
+sudo chown  -R $USER:$USER code
+sudo chmod -R 777 code
